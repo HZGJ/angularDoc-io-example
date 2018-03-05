15ea096a571749a8c54adb9106e79c6e5ba62cdc
GuanglongDu <duguanglong002@163.com>
2018-03-05 22:33:54 +0800

    test2

--- src/app/welcome.component.ts
+++ src/app/welcome.component.ts
@@ -6,7 +6,6 @@ import { UserService }       from './model/user.service';
   selector: 'app-welcome',
   template: '<h3 class="welcome" ><i>{{welcome}}</i></h3>'
 })
-
 export class WelcomeComponent  implements OnInit {
   welcome = '-- not initialized yet --';
   constructor(private userService: UserService) { }
